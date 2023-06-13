c5ab660cbeb7109d08e41d7f69535b790614f05c
Rom213 <romarioariza@outlook.es>
2023-06-12 21:53:41 -0500

    update

--- src/component/modoNocturno/Night.tsx
+++ src/component/modoNocturno/Night.tsx
@@ -1,43 +1,33 @@
-import { useEffect, useState } from "react";
-import './night.css'
-import '../Mobile/style.css'
-import '../computer/computer.css'
-import '../../App.css'
-import globalStore from "../../secstore/Store_global";
-
-
-
+import React, { useEffect, useState } from "react";
+import './night.css';
+import '../Mobile/style.css';
+import '../computer/computer.css';
+import '../../App.css';
 
 const Night: React.FC = () => {
-
-const { ThunkValue } = globalStore()
-useEffect(() => {
-  setChecked(true)
-}, [])
   const [checked, setChecked] = useState<boolean>(
     localStorage.getItem("theme") === "dark" ? true : false
   );
 
-  
   useEffect(() => {
     document
       .getElementsByTagName("HTML")[0]
       .setAttribute("data-theme", localStorage.getItem("theme") || "");
-
   }, [checked]);
 
+  useEffect(() => {
+    setChecked(localStorage.getItem("theme") === "dark" ? true : false);
+  }, []);
+
   const toggleThemeChange = (): void => {
-    if (checked === false) {
-      localStorage.setItem("theme", "dark");
-      setChecked(true);
-    } else {
+    if (checked) {
       localStorage.setItem("theme", "light");
       setChecked(false);
+    } else {
+      localStorage.setItem("theme", "dark");
+      setChecked(true);
     }
-    ThunkValue()
   };
-
-  
   
 
   return (
