a374b0ebef15bce604439909bafe9f71460942a7
rom213 <romarioariza@outlook.es>
2024-02-11 17:45:37 -0500

    commit change mobile

--- src/component/Mobile/Moible.tsx
+++ src/component/Mobile/Moible.tsx
@@ -1,17 +1,10 @@
-import useScroll from "../../hooks/windowScroll";
 import "./style.css";
-import imgenT from "../../assets/proyects/tienedadetecnologia.png";
-import pokedex1 from "../../assets/proyects/pokedex1.png";
-import rick from "../../assets/proyects/rickandmorty.png";
-import restaurants from "../../assets/proyects/reRestaurant.png";
-import imgBlog from "../../assets/proyects/blog.png";
 import Skils from "../skills/Skils";
 import Contact from "../contact/Contact";
 import Night from "../modoNocturno/Night";
+import data from "../../json/data.json";
 
 const Moible = () => {
-  const { scrollY } = useScroll();
-
   return (
     <div className="ContentMobile">
       <div className="noche">
@@ -42,267 +35,111 @@ const Moible = () => {
         </div>
       </div>
 
+
+
       <div className="secMobile2">
         <div className="headerAbout">
           <h2>About</h2>
         </div>
 
         <div className="textAbout">
-          I am a seasoned full stack programmer with expertise in frontend
-          technologies such as JavaScript, CSS, HTML, React, and TypeScript. On
-          the backend, I specialize in API development, socket handling,
-          authentication, environment variables, error management, and working
-          with relational databases, particularly PostgreSQL.
-          <br />
-          <br />
-          My commitment to staying up-to-date with the latest industry trends
-          ensures that I can provide innovative and efficient solutions for any
-          project. I am constantly exploring new technologies and techniques to
-          enhance the quality and performance of my work.
-          <br />
-          <br />
-          With my extensive knowledge and experience, I am well-equipped to
-          tackle diverse challenges and deliver high-quality software solutions.
-          I prioritize clean and maintainable code, ensuring scalability and
-          robustness in my applications.
-          <br />
-          Whether it's building responsive and intuitive user interfaces or
-          developing robust and secure backend systems.
+          {data.español.description.description}
         </div>
       </div>
 
-      <div className="contentProyects">
-        <div className="headerAbout">
-          <h2>Proyects</h2>
+      <div className="cont_expe">
+        <div className="container">
+          <div className="headerAbout">
+            <h2>experience</h2>
+          </div>
+          {data.español.experience.map((info) => {
+            return (
+              <div className="card">
+                <div className="face0 face1" style={{ height: "210px" }}>
+                  <div className="cardExperience">
+                    <div style={{ width: "300px" }}>
+
+                      <div>{info.fecha}</div>
+
+                      <img style={{ width: "150px", position: "relative", top: "10px" }} src="https://firebasestorage.googleapis.com/v0/b/blog-46e71.appspot.com/o/portafolio%2Facademlo.jpg?alt=media&token=345a8306-5eb2-4fe0-a50f-2b5ecab88bfa" alt="" />
+                    </div>
+                    <div>
+                      <div className="contentInfo">
+                        <h4>{info.company}</h4>
+                        <h4>{info.position}</h4>
+                      </div>
+
+                      <br />
+                      <div className="contenSkills">
+                        {info.skills.map((info2) => {
+                          return <div className="contenSkill">
+                            {info2.name}
+                          </div>
+                        })
+                        }
+                      </div>
+
+                      <div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div className="face0 face2">
+                  <div className="contentBack">
+                    <p style={{ whiteSpace: "pre-wrap", textAlign: "justify" }}>{info.description}</p>
+                    <div className="contentLinks">
+                      <div className="pointerCursor">
+                        <i className='bx bx-paper-plane bx-fade-right bx-md'></i>
+                      </div>
+                      <div className="contLinks">
+                        <i className='bx bx-link'><a href="https://certiwise.co">CertiWise</a></i>
+                        <i className='bx bx-link'><a href="https://certiwise.co/verify/14">certificate</a></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            );
+          })}
         </div>
-        <div className="contentProyectsFront">
-          <div className="contentProyec">
-            <br />
-            <br />
-            <h3 className="titleproy">frontEnd</h3>
-            <a
-              className="linkEcomeTec"
-              href="https://tiendatecnologica.netlify.app/"
-            >
-              Ecomerse of technology store
-            </a>
-            <div className="contentText">
-              Se utilizó la tecnología de React con TypeScript para desarrollar
-              un diseño intuitivo y atractivo con el objetivo de que el usuario
-              se sienta cómodo. Se implementaron rutas y rutas protegidas para
-              usuarios logueados, y se gestionaron las bibliotecas de Redux,
-              Axios, Zustand y otras más. El proyecto aún está en desarrollo,
-              donde se está implementando React Query para el manejo de caché y
-              se está añadiendo capacidad de respuesta para dispositivos
-              móviles.
-            </div>
-            {scrollY > 950 && (
-              <div className="contentAnima">
-                <img className="imgTecno" src={imgenT} alt="" />
-                <div className="contentSkills">
-                  <div className="skill">react Ts</div>
-                  <div className="skill">redux</div>
-                  <div className="skill">Zustand</div>
-                  <div className="skill">Api</div>
+
+        <div className="container">
+          <div className="headerAbout">
+            <h2>Projects</h2>
+          </div>
+          {data.español.projects.map((dat) => {
+            return (
+              <div className="card">
+                <div className="face0 face1">
+                  <div className="content0">
+                    <div>
+                      <img className="imgCo" src={`${dat.img_link}`} alt="" />
+                      <h3>{dat.title}</h3>
+                    </div>
+                    <div className="contentSkill">
+                      {dat?.skill.map((ski) => {
+                        return <div className="skil"><img className="skikImg" src={`${ski.name}`} alt="" /></div>
+                      })}
+                    </div>
+                  </div>
+                </div>
+                <div className="face0 face2">
+                  <div className="" style={{ textAlign: "justify" }}>
+                    <p>{dat.description}</p>
+                  </div>
                 </div>
               </div>
-            )}
-          </div>
-
-          <div className="contentProyec">
-            <br />
-            <a
-              className="linkEcomeTec"
-              href="https://worldrickandmorty.netlify.app"
-            >
-              World Rick and Morty
-            </a>
-            <div className="contentText">
-              En el proyecto se ha incorporado un buscador similar al de Google,
-              que despliega opciones de búsqueda a medida que se escribe.
-              Además, se ha agregado un modo noche sencillo para mejorar la
-              estética. Para la implementación de la paginación, se ha creado la
-              funcionalidad correspondiente. Adicionalmente, se ha realizado el
-              consumo de la API de Rick and Morty.
-            </div>
-
-            {scrollY > 1400 && (
-              <div className="contentAnima">
-                <img className="imgTecno" src={`${rick}`} alt="" />
-                <div className="contentSkills">
-                  <div className="skill">react Js</div>
-                  <div className="skill">Api</div>
-                  <div className="skill">axios</div>
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="contentProyec">
-            <br />
-            <a
-              className="linkEcomeTec"
-              href="https://pokeedexsite.netlify.app/"
-            >
-              Pokedex
-            </a>
-            <div className="contentText">
-              Se utilizó la API pública PokeAPI para obtener datos de Pokémon.
-              Para realizar las solicitudes HTTP a la API, se utilizó la
-              biblioteca Axios. Además, se trabajó con rutas y rutas protegidas
-              utilizando la biblioteca react-router-dom. Para gestionar el
-              estado de la aplicación, se utilizó la biblioteca Redux Toolkit.
-              Se aplicó la reutilización de código para mejorar la optimización
-              de la aplicación.
-            </div>
-            {scrollY > 2000 && (
-              <div className="contentAnima">
-                <img className="imgTecno" src={`${pokedex1}`} alt="" />
-                <div className="contentSkills">
-                  <div className="skill">react Js</div>
-                  <div className="skill">redux</div>
-                  <div className="skill">routes</div>
-                  <div className="skill">Api</div>
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="contentProyec">
-            <br />
-            <hr />
-            <br />
-            <br />
-            <h3 className="titleproy">BackEnd</h3>
-            <a
-              className="linkEcomeTec"
-              href="https://apirestaurant.onrender.com/api/v1/docs/"
-            >
-              AppRestaurant
-            </a>
-
-            <div className="contentText">
-              En este proyecto se desarrollo una API (Interfaz de Programación
-              de Aplicaciones) para la gestión de restaurantes en el backend. La
-              API permitirá la administración de reservaciones, usuarios,
-              comidas, pedidos y comentarios sobre la calidad del servicio en
-              los restaurantes. Además, se brinda la funcionalidad de crear
-              usuarios, lo cual les otorga diversas acciones, como realizar
-              pedidos y dejar comentarios.
-              <br />
-              Tecnologías utilizadas: en la implementación de este proyecto, se
-              uso una base de datos mySql relaciones entre tablas ,se ha hecho
-              uso del framework Express, conocido por su eficiencia y facilidad
-              de uso en el desarrollo de aplicaciones web y APIs. Asimismo, se
-              ha aplicado el manejo de errores para garantizar una experiencia
-              fluida y confiable. Las variables de entorno han sido utilizadas
-              para configurar aspectos específicos de la aplicación según el
-              entorno de despliegue. Por último, se ha implementado un sistema
-              de autenticación y niveles de acceso para garantizar la seguridad
-              y la privacidad de los usuarios.
-            </div>
-
-            {scrollY > 100 && (
-              <div className="contentAnima">
-                <img className="imgTecno" src={`${restaurants}`} alt="" />
-                <div className="contentSkills">
-                  <div className="skill">nodejs</div>
-                  <div className="skill">express</div>
-                  <div className="skill">postman</div>
-                  <div className="skill">mySql</div>
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="contentProyec">
-            <br />
-            <br />
-            <a
-              className="linkEcomeTec"
-              href="https://blogswagger.onrender.com/api/v1/docs/"
-            >
-              blogPosts
-            </a>
-            <div className="contentText">
-              Este proyecto se centra en el desarrollo de un blog en el que los
-              usuarios pueden publicar sus posts y compartirlos con otros
-              usuarios de la aplicación. Cualquier persona puede registrarse e
-              iniciar sesión utilizando su correo electrónico para crear
-              publicaciones y comentar los posts de otros usuarios. Para la
-              implementación, se utilizó el framework Express, el cual facilitó
-              la creación de la Api.
-              <br />
-              Se incorporaron sockets para permitir la visualización en tiempo
-              real de las publicaciones que van realizando los usuarios. Además,
-              se empleó Firebase para almacenar las imágenes como enlaces en la
-              base de datos MySQL.
-              <br />
-              En cuanto a la seguridad, se implementó la encriptación de
-              contraseñas para garantizar la protección de los usuarios. También
-              se establecieron relaciones en la base de datos MySQL para
-              gestionar las interacciones entre los distintos elementos del
-              blog.
-              <br />
-              <br />
-              En resumen, este proyecto se enfocó en desarrollar un blog con
-              funcionalidades de publicación, comentarios en tiempo real y
-              gestión de usuarios, utilizando el framework Express, sockets,
-              Firebase y MySQL, entre otras herramientas.
-            </div>
-            {scrollY > 1500 && (
-              <div className="contentAnima">
-                <img className="imgTecno" src={`${imgBlog}`} alt="" />
-                <div className="contentSkills">
-                  <div className="skill">nodejs</div>
-                  <div className="skill">express</div>
-                  <div className="skill">bycrip</div>
-                  <div className="skill">sockets</div>
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="contentProyec">
-            <br />
-            <br />
-            <a
-              className="linkEcomeTec"
-              href="https://swaggerpelis.onrender.com/api/v1/docs/"
-            >
-              appi-pelis
-            </a>
-
-            <div className="contentText">
-              Este proyecto fue creado para ser aplicado en un frontend. Es una
-              aplicación donde los usuarios pueden registrarse y acceder
-              mediante un token. Las películas, géneros y personajes tienen sus
-              respectivas imágenes, las cuales se cargan en Firebase. se
-              establecio una base de datos actualizada MySQL2 que tiene sus
-              respectivas migraciones y seeders, además de tener relaciones
-              entre tablas de 1 a 1, 1 a muchos y muchos a muchos.
-            </div>
-            <div className="contentAnima">
-              <img
-                className="imgTecno"
-                src="https://th.bing.com/th/id/R.5eb2d62cac6c08719b4d3b8672a7e56f?rik=Q4bbJqLRMrfKeQ&riu=http%3a%2f%2ffrikilogia.com%2fwp-content%2fuploads%2f2012%2f01%2ftecnologia-organiza-tu-coleccion-de-peliculas-la-b.jpg&ehk=84rPabYc5fidYt3aWsm5rHpK7QJvGlA0pfWBeV4NPiw%3d&risl=&pid=ImgRaw&r=0"
-                alt=""
-              />
-              <div className="contentSkills">
-                <div className="skill">nodejs</div>
-                <div className="skill">express</div>
-                <div className="skill">bycrip</div>
-                <div className="skill">Api</div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
+
       </div>
-
       <div className="contentAlldesCon">
-        <br />
-        <br />
-        <h1>Skills</h1>
+        <div className="headerAbout">
+          <h2>Skills</h2>
+        </div>
         <Skils />
       </div>
       <div className="contentDescription">
